97284683e972cd4807d4a378ec6ebf7905a7a7f0
RochaEverton <rochaevertondev@gmail.com>
2025-08-06 14:46:30 -0300

    create form

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,30 @@ export default function Home() {
       <div className="container px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-6">
-            <img src="/link.jpg" alt="imagem link" className="d-block mx-lg-auto img-fluid" width="700" height="500"/>
+            <img src="/link.jpg" alt="imagem link" className="d-block mx-lg-auto img-fluid" width="700" height="500" />
           </div>
           <div className="col-6">
             <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Link Protect</h1>
-
+            <p className="lead">Proteja seus links. Lucre com eles.</p>
+            <hr />
+            <p>Cole a sua URL abaixo, defina a taxa por clique e conecte sua carteira para proteger seu link com a tecnologia blockchain.</p>
+            <div className="form-floating mb-3">
+              <input type="text" id="url" className="form-control" />
+              <label htmlFor="url">Link:</label>
+            </div>
+            <div className="row mb-3">
+              <div className="col-6">
+                <div className="form-floating">
+                  <input type="number" id="fee" className="form-control" />
+                  <label htmlFor="fee">Taxa por clique (wei):</label>
+                </div>
+              </div>
+              <div className="col-6">
+                <button type="button" className="btn btn-primary w-100 h-100">
+                  Conectar e criar Link
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
