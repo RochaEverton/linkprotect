737e9a97cc93a460f444863fe632f9e704a45679
RochaEverton <rochaevertondev@gmail.com>
2025-08-06 14:30:31 -0300

    add imagem link homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
 
 export default function Home() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <div className="container px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-6">
+            <img src="/link.jpg" alt="imagem link" className="d-block mx-lg-auto img-fluid "/>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
