735f15990a721d719ad158b004c848f91915edbf
RochaEverton <rochaevertondev@gmail.com>
2025-08-06 14:34:23 -0300

    add titulo

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,11 @@ export default function Home() {
       <div className="container px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-6">
-            <img src="/link.jpg" alt="imagem link" className="d-block mx-lg-auto img-fluid "/>
+            <img src="/link.jpg" alt="imagem link" className="d-block mx-lg-auto img-fluid" width="700" height="500"/>
+          </div>
+          <div className="col-6">
+            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Link Protect</h1>
+
           </div>
         </div>
       </div>
