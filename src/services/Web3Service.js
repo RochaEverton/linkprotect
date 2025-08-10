e7bfdaffd53204ffb79c6a457660165fb19c3283
RochaEverton <rochaevertondev@gmail.com>
2025-08-10 20:49:49 -0300

    add function request autorization from user

--- src/services/Web3Service.js
+++ src/services/Web3Service.js
@@ -4,4 +4,5 @@ async function connectContract(){
     if(!window.ethereum) throw new Error("Sem MetaMask instalada");
 
     const web3 = new Web3(window.ethereum)
+    const accounts = await web3.eth.requestAccounts();
 }
